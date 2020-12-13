b4ba85b37c70a319f0500ba972ef81e742f4f356
JacksieCheung <1799902904@qq.com>
2020-12-13 23:45:26 +0800

    feat:list handle

--- src/list.c
+++ src/list.c
@@ -1,2 +1,91 @@
 #include "errno.h"
+#include "list.h"
+#include<string.h>
 
+// 初始化 DB
+Status InitDB(){
+	DB = (listHead*)malloc(sizeof(listHead));
+	if (DB==NULL) return Error;
+	return OK;
+}
+
+/*Status InsertDBList(int index,listNode* node){
+	if (DB->Nums<index) return Error;
+
+	listNode* tmp = DB->Next;
+	for (int i=0;i<index;i++){
+		tmp = tmp->Next;
+	}
+
+	node->Next = NULL;
+	tmp->Next = node;
+
+	DB->Nums++;
+	return OK;
+}*/
+
+// 查找数据库，找到返回指向它的指针，找不到返回 NULL
+listNode* SearchDBByName(const char* Name){
+	listNode* tmp = DB->Next;
+	
+	while(tmp!=NULL){
+		if (strcmp(tmp->Name,Name)){
+			return tmp;
+		}
+	}
+
+	return NULL;
+}
+
+// 商店进货，先查找商品，再插入
+Status AddData(altRequest* ptr){
+	if (ptr==NULL) return Error;
+
+	listNode* tmp = SearchDBByName(ptr->Name);
+	if (tmp==NULL) return Error; // record not found
+
+	tmp->Sum += ptr->Sum;
+	return OK;
+}
+
+// 新增商品，先查找，再插入
+Status AddDataNotExist(altRequest* ptr){
+	if (ptr==NULL) return Error;
+
+	listNode* tmp = SearchDBByName(ptr->Name);
+	if (tmp!=NULL) return Error; // record is existed
+
+	tmp = (listNode*)malloc(sizeof(listNode));
+	if (tmp==NULL) return Error;
+
+	strcpy(tmp->Name,ptr->Name);
+	tmp->Sum = ptr->Sum;
+	tmp->Next = DB->Next;
+	DB->Next = tmp;
+
+	return OK;
+}
+
+// 商品--,先查找，再改。resp 返回 sum 为 0 的节点，用于设置定时任务
+Status DelData(altRequest* ptr,listNode* resp){
+	if (ptr==NULL||resp!=NULL) return Error;
+
+	listNode* tmp = SearchDBByName(ptr->Name);
+	if (tmp==NULL) return Error; // record not found
+
+	if (tmp->Sum<ptr->Sum) return Error; // sum overflow
+
+	tmp->Sum = tmp->Sum - ptr->Sum;
+
+	if (tmp->Sum==0){
+		resp = tmp;
+		return Error; // sum = 0
+	}
+
+	return OK;
+}
+
+// 从链表删除，此为定时任务
+Status DelDataFromList(listNode* node){
+	
+}
--- src/list.h
+++ src/list.h
@@ -3,21 +3,32 @@
 #include "errno.h"
 
 // --structure--
-
+// 节点结构
 typedef struct listNode{
-	int ID;     // 唯一标志
 	char* Name; // 名字
 	int Sum;    // 总数
+	struct listNode* Next;
 }listNode;
 
-listNode* DB=NULL;
+// 头结构
+typedef struct listHead{
+	int Nums;       // 成员个数
+	listNode* Next; // 指向孩子
+}listHead;
+
+// 数据库变量
+listHead* DB=NULL;
+
+// --request--
+// 进货请求
+typedef struct altRequest{
+	char* Name;
+	int Sum;
+}altRequest;
 
 // --func--
 // 初始化链表，malloc
-Status InitDB(){
-	DB = (listNode*)malloc(sizeof(listNode));
-	if (DB==NULL) return Error;
-	return OK;
-}
+Status InitDB();
 
 // 插入表
+Status InsertDB(int index,listNode* node);
